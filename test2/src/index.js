63b3edded647fdb9fae8c11f209775be701f366a
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-19 10:07:03 +0800

    添加样式

--- test2/src/index.js
+++ test2/src/index.js
@@ -1,10 +1,11 @@
-
+import './style.css';
 import _ from 'lodash';
 function component() {
   var element = document.createElement('div');
 
   //Lodash，现在由 import _ from 'lodash'导入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
