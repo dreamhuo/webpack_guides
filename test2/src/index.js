e4011a1d741ddedb88b8367a03f9d43b77ffae68
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-19 13:55:09 +0800

    添加file-loader

--- test2/src/index.js
+++ test2/src/index.js
@@ -1,11 +1,18 @@
 import './style.css';
 import _ from 'lodash';
+import Icon from './icon.png';
+
 function component() {
   var element = document.createElement('div');
 
   //Lodash，现在由 import _ from 'lodash'导入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们现有的div
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
