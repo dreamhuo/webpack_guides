a8477ee5a837550b0a0d9038202ddb611ce03375
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-19 14:26:59 +0800

    添加file-loader

--- test2/webpack.config.js
+++ test2/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'none',  // "development" | "production" | "none"
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -17,6 +18,12 @@ module.exports = {
         },
         {
             test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader'
+            ]
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
             use: [
                 'file-loader'
             ]
