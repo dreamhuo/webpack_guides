7d131f33ced9f632691bcbd676e80033eb99de11
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-20 16:52:28 +0800

    添加 tree shaking 代码示例

--- test6/webpack.config.js
+++ test6/webpack.config.js
@@ -1,31 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',  // "development" | "production" | "none"
-  entry: {
-    app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-        title: '模块热替换'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   }
