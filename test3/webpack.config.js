231c12b490cd1ba932cb162ba4ea27ee4908dbcc
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-20 11:13:19 +0800

    修改配置

--- test3/webpack.config.js
+++ test3/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'none',  // "development" | "production" | "none"
@@ -8,6 +9,7 @@ module.exports = {
     print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
         title: '管理输出'
     })
