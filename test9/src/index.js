b70af873997d2b162c88dae8397b68d4c55478cc
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-21 16:17:01 +0800

    代码分离，动态导入示例

---
+++ test9/src/index.js
@@ -0,0 +1,14 @@
+
+function getComponent() {
+    var element = document.createElement('div');
+    return import(/* webpackChunkName: 'lodash' */ 'lodash').then(_ => {
+        var element = document.createElement('div');
+        var _ = _.default;
+        element.innerHTML = _.join(['Hello', ' webpack'], '');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
+}
+
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
--- test9/webpack.config.js
+++ test9/webpack.config.js
@@ -5,22 +5,17 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',  // "development" | "production" | "none"
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js'
+    index: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-        title: '代码分离'
+        title: '动态导入'
     })
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
   }
 };
