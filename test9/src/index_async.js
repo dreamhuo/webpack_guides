3cbf9b8ee4fb75a56a2fdb3ab34726f84f362061
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-21 16:27:10 +0800

    代码分离，动态导入示例

--- test9/src/index_async.js
+++ test9/src/index_async.js
@@ -2,10 +2,11 @@
 async function getComponent() {
     var element = document.createElement('div');
     const _ = await import(/* webpackChunkName: 'lodash' */ 'lodash');
-    element.innerHTML = _.join(['Hello', ' Hello', ' webpack'], '');
+    element.innerHTML = _.join(['Hello1212', ' Hello', ' webpack'], '');
     return element;
 }
-
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+setTimeout(() => {
+    getComponent().then(component => {
+        document.body.appendChild(component);
+    });
+}, 5000);
