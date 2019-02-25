09fe8e991222d8a9e235900ac57b75ec0b819135
霍长生 <huo8008@126.com>
2019-02-25 22:52:27 +0800

    shimming示例

---
+++ test13/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  // mode: 'production',  // "development" | "production" | "none"
+  entry: './src/index.js',
+  plugins: [
+    // 如果你遇到了至少一处用到 _ 变量的模块实例，
+    // 那请你将 lodash package 包引入进来，并将其提供给需要用到它的模块
+    new webpack.ProvidePlugin({
+        _: 'lodash'
+        // 这样就能很好的与 tree shaking 配合，将 lodash 库中的其他没用到的部分去除
+        // join: ['lodash', 'join']
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
