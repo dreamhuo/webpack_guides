aa722c6bc847be34325eef8975abc1cc917c00e9
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-25 17:38:41 +0800

    创建一个 library

--- test12/src/index.js
+++ test12/src/index.js
@@ -1,5 +1,30 @@
 import _ from 'lodash';
-import numRef from './ref.json';
+const numRef =   [
+    {
+      'num': 1,
+      'word': 'One'
+    },
+    {
+      'num': 'huo2',
+      'word': 'Two'
+    },
+    {
+      'num': 3,
+      'word': 'Three'
+    },
+    {
+      'num': 4,
+      'word': 'Four'
+    },
+    {
+      'num': 5,
+      'word': 'Five'
+    },
+    {
+      'num': 0,
+      'word': 'Zero'
+    }
+  ];
 
 export function numToWord(num) {
     return _.reduce(numRef, (accum, ref) => {
--- test12/webpack.config.js
+++ test12/webpack.config.js
@@ -6,5 +6,15 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'webpack-numbers.js'
+    },
+    // 这意味着你的 library 需要一个名为 lodash 的依赖，这个依赖在用户的环境中必须存在且可用。
+    // 如果你计划只是将 library 用作另一个 webpack bundle 中的依赖模块，则可以将 externals 指定为数组。
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
 }
